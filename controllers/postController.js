4e587ba4bed63a231ce08ebd1cc37d56bda8bac5
Moamen <moamen7a@gmail.com>
2025-05-03 21:55:12 +0300

    test variable in members route

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,8 +3,8 @@ const Post = require("../models/PostModel")
 const { excludeFieldsFilter } = require("./../utils/filterReqBody")
 const catchAsync = require("../utils/catchAsync")
 
-exports.createPost = factory.createOne(Post, excludeFieldsFilter, ["user"], (req, filter)=>{
-    filter.user = req.user.id
+exports.createPost = factory.createOne(Post, excludeFieldsFilter, ["user"], (req, filter) => {
+    filter.user = req.body.id// req.user.id
 })
 exports.getPost = factory.getOne(Post)
 exports.getPosts = factory.getAll(Post)
